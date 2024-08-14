e7412e90e534ba3d1d9a8bd33b8e9ca3146d66c7
Keks <keks@htmlacademy.ru>
2024-08-14 20:53:53 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/requestModule.js
@@ -0,0 +1,42 @@
+import { renderGallery } from './gallery';
+import {showingDownloadError, showingSuccess, showingError} from './util.js';
+
+
+const requestDate = () => {
+  fetch('https://32.javascript.htmlacademy.pro/kekstagram/data')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error();
+    })
+    .then((date) => renderGallery(date))
+    .catch(() => {
+      showingDownloadError();
+    });
+};
+
+const sendingData = (imgUploadForm) => {
+  fetch('https://32.javascript.htmlacademy.pro/kekstagram1',
+    {
+      method: 'POST',
+      //mode: 'no-cors',
+      body: new FormData(imgUploadForm),
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error();
+    })
+    .then(() => {
+      showingSuccess();
+    })
+    .catch(() => {
+      showingError();
+    });
+};
+
+export {requestDate, sendingData};
+
